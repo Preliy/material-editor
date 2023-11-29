47daba4c71185933cfad54c9a26de152f6fe05d1
Preliy <vikt.gaponenko@gmail.com>
2023-11-29 21:01:24 +0100

    fix: save material changes in panel

--- Editor/Scripts/MaterialPalette.cs
+++ Editor/Scripts/MaterialPalette.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Preliy.Utils
 {
-    [CreateAssetMenu]
+    [CreateAssetMenu(fileName = "Custom Material Palette", menuName = "Material Editor/Custom Material Palette", order = 2000)]
     public class MaterialPalette : ScriptableObject
     {
         public Material[] Materials => _materials;
@@ -14,6 +14,11 @@ namespace Preliy.Utils
         {
             get => _materials[i];
             set => _materials[i] = value;
+        }
+
+        public void Reset()
+        {
+            _materials = new Material[10]; 
         }
     }
 }
